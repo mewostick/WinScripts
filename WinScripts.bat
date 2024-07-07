a5dca1e8592ca4a5e2737ea494db9f85d7b0df91
Mewo <72226666+mewostick@users.noreply.github.com>
2024-07-07 18:00:36 +0200

    1.1 changes

--- WinScripts.bat
+++ WinScripts.bat
@@ -3,14 +3,13 @@
 cls
 title WinScripts - main menu
 echo.
-echo. WinScripts 1.0
+echo. WinScripts 1.1
 echo. A collection of tweaks and PowerShell scripts.
 echo. https://github.com/mewostick/WinScripts
 echo.
 echo. !!! MAKE SURE THIS IS BEING RAN AS ADMINISTRATOR !!!
 echo.
-echo. Type in "debloat" if you want to run my $hitty optimizer
-echo. Type in "tweaks" for tweaks
+echo. Type in "tweaks" for registry tweaks
 echo. Type in "scripts" for PowerShell scripts
 echo. Type in "cpanel" for certain Control Panel applets
 echo. Type in "notes" for notes
@@ -22,8 +21,7 @@ echo. Type in "exit" to exit
 echo.
 set /p typein= Enter your choice: 
 
-if %typein% == debloat goto debloat
-if %typein% == exit goto bye
+if %typein% == exit goto exit
 if %typein% == notes goto notes
 if %typein% == matrix goto matrix
 if %typein% == tweaks goto tweaks
@@ -42,14 +40,6 @@ echo.
 pause
 cls
 goto start
-
-:bye
-cls
-title WinScripts - quitting..
-echo.
-echo. You did not want to continue, therefore this script will quit.
-timeout 3
-exit
 
 :light
 color F0
@@ -85,126 +75,6 @@ pause
 cls
 goto start
 
-:debloat 
-cls
-title WinScripts - $hitty optimizer is running!
-echo. Running!
-echo.
-echo Uninstalling OneDrive (32-bit)
-taskkill /f /im OneDrive.exe
-%SystemRoot%\System32\OneDriveSetup.exe /uninstall
-echo Uninstalling OneDrive (64-bit)
-%SystemRoot%\SysWOW64\OneDriveSetup.exe /uninstall
-echo Services:
-echo Disabling DiagTrack
-sc stop DiagTrack
-sc config DiagTrack start= disabled
-echo Disabling Delivery Optimization
-sc stop DoSvc
-sc config DoSvc start= disabled
-echo Disabling Server
-echo.
-echo. Telemetry services have been disabled.
-echo.
-echo. Scheduled tasks:
-echo.
-echo Disabling Microsoft Compatibility Appraiser taskkill (telemetry)
-schtasks /Change /TN "Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser" /Disable
-echo Disabling ProgramDataUpdater (telemetry)
-schtasks /Change /TN "Microsoft\Windows\Application Experience\ProgramDataUpdater" /Disable
-echo Disabling StartupAppTask (telemetry)
-schtasks /Change /TN "Microsoft\Windows\Application Experience\StartupAppTask" /Disable
-echo Disabling Consolidator (telemetry)
-schtasks /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\Consolidator" /Disable
-echo Disabling KernelCeipTask (telemetry)
-schtasks /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\KernelCeipTask" /Disable
-echo Disabling UsbCeip (telemetry)
-schtasks /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\UsbCeip" /Disable
-echo Disabling Scheduled Diagnosis (telemetry)
-schtasks /Change /TN "Microsoft\Windows\Diagnosis\Scheduled" /Disable
-echo Disaling Disk Diagnostic Data Collector (telemetry)
-schtasks /Change /TN "Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticDataCollector" /Disable
-echo Disabling Disk Footprint Diagnostics (telemetry)
-schtasks /Change /TN "Microsoft\Windows\DiskFootprint\Diagnostics" /Disable
-echo Disabling Storage Sense
-schtasks /Change /TN "Microsoft\Windows\DiskFootprint\StorageSense" /Disable
-echo Disabling dusmtask
-schtasks /Change /TN "Microsoft\Windows\DUSM\dusmtask" /Disable
-echo Disabling DmClient (telemetry)
-schtasks /Change /TN "Microsoft\Windows\Feedback\Siuf\DmClient" /Disable
-echo Disabling DmClientOnScenarioDownload (telemetry)
-schtasks /Change /TN "Microsoft\Windows\Feedback\Siuf\DmClientOnScenarioDownload" /Disable
-echo Disabling WinSAT
-schtasks /Change /TN "Microsoft\Windows\Maintenance\WinSAT" /Disable
-echo Disabling BfeOnServiceStartTypeChange
-schtasks /Change /TN "Microsoft\Windows\Windows Filtering Platform\BfeOnServiceStartTypeChange" /Disable
-echo Disabling Sqm-Tasks (telemetry)
-schtasks /Change /TN "Microsoft\Windows\PI\Sqm-Tasks" /Disable
-echo Disabling BackgroundUploadTask
-schtasks /Change /TN "Microsoft\Windows\SettingSync\BackgroundUploadTask" /Disable
-echo Disabling GatherNetworkInfo (telemetry)
-schtasks /Change /TN "Microsoft\Windows\NetTrace\GatherNetworkInfo" /Disable
-echo Disabling Device (telemetry)
-schtasks /Change /TN "Microsoft\Windows\Device Information\Device" /Disable
-echo.
-echo. Rubbish tasks have been disabled
-echo.
-echo. Registry
-echo.
-echo Disallowing telemetry
-REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows\DataCollection" /v "AllowTelemetry" /t REG_DWORD /d 0
-REG ADD "HKCU\SOFTWARE\Policies\Microsoft\Windows\DataCollection" /v "AllowTelemetry" /t REG_DWORD /d 0
-echo Disabling Windows Customer Experience Improvement Program
-REG ADD "HKLM\SOFTWARE\Policies\Microsoft\SQMClient"
-REG ADD "HKLM\SOFTWARE\Policies\Microsoft\SQMClient\Windows"
-REG ADD "HKLM\SOFTWARE\Policies\Microsoft\SQMClient\Windows" /v "CEIPEnable" /t REG_DWORD /d 0
-echo Disabling Cortana
-REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search"
-REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v "AllowCortana" /t REG_DWORD /d 0
-echo Disabling Windows Error Reporting
-REG ADD "HKLM\SOFTWARE\Microsoft\Windows\Windows Error Reporting" /v "Disabled" /t REG_DWORD /d 1
-echo Disabling Steps Recorder
-REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows\AppCompat"
-REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows\AppCompat" /v "DisableUAR" /t REG_DWORD /d 1
-echo Disabling Inventory Collector
-REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows\AppCompat" /v "DisableInventory" /t REG_DWORD /d 1
-echo Disabling Handwriting automatic learning
-REG ADD "HKLM\SOFTWARE\Policies\Microsoft\InputPersonalization"
-REG ADD "HKLM\SOFTWARE\Policies\Microsoft\InputPersonalization" /v "RestrickImplicitTextCollection" /t REG_DWORD /d 1
-REG ADD "HKLM\SOFTWARE\Policies\Microsoft\InputPersonalization" /v "RestrictImplicitInkCollection" /t REG_DWORD /d 1
-echo Disabling Advertising ID
-REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows\AdvertisingInfo"
-REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows\AdvertisingInfo" /v "DisabledByGroupPolicy" /t REG_DWORD /d 1
-echo Disabling Search Companion
-REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows\SearchCompanion"
-REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows\SearchCompanion" /v "DisableContentFileUpdates" /t REG_DWORD /d 1
-echo Disabling Microsoft consumer exeperiences
-REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows\CloudContent"
-REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows\CloudContent" /v "DisableWindowsConsumerFeatures" /t REG_DWORD /d 1
-echo.
-echo. Registry done!
-echo.
-title WinScripts - done!
-echo. Script's done!
-echo. It may be recommended to restart your computer now.
-echo. 
-echo. You're free to go to the main menu now.
-pause
-goto done
-
-:done
-title WinScripts - choose!
-echo.
-echo. Type in "restart" to restart your machine
-echo. Type in "exit" to quit the program
-echo. Type in "start" to go back to the main menu
-echo.
-set /p done= Enter your choice: 
-
-if %done% == restart goto restart
-if %done% == exit goto exit
-if %done% == start goto start
-
 goto wrongthesecond
 :wrongthesecond
 cls
@@ -231,17 +101,19 @@ exit
 
 :tweaks
 cls
-title WinScripts - tweaks
+title WinScripts - registry tweaks
 echo.
 echo. TWEAKS
 echo.
 echo. Type in "1" to increase JPEG quality to 100% for wallpapers
 echo. Type in "2" to enable verbose messages
 echo. Type in "3" to disable MRT updates
+echo. Type in "4" to enable Ctrl+Alt+Delete on lock screen
 echo.
 echo. Type in "d1" to remove JPEG quality increase
 echo. Type in "d2" to disable verbose messages
-echo. Typ3 in "d3" to enable MRT updates
+echo. Type in "d3" to enable MRT updates
+echo. Type in "d4" to disable Ctrl+Alt+Delete on lockscreen
 echo.
 echo. Type in "start" to go to the main menu
 echo. Type in "restart" to restart your machine
@@ -252,9 +124,11 @@ set /p optionaltweaks= Enter your choice:
 if %optionaltweaks% == 1 goto 1
 if %optionaltweaks% == 2 goto 2
 if %optionaltweaks% == 3 goto 3
+if %optionaltweaks% == 4 goto 4
 if %optionaltweaks% == d1 goto d1
 if %optionaltweaks% == d2 goto d2
 if %optionaltweaks% == d3 goto d3
+if %optionaltweaks% == d4 goto d4
 if %optionaltweaks% == start goto start
 if %optionaltweaks% == restart goto restart
 if %optionaltweaks% == exit goto exit
@@ -312,6 +186,20 @@ echo.
 timeout 5
 goto tweaks
 
+:4
+cls
+title WinScripts - applying the tweak..
+echo.
+echo. Enabling Ctrl+Alt+Del sign-in on Windows lock screen..
+echo.
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v "DisableCAD" /t REG_DWORD /d 0 /f
+echo.
+echo. Ctrl+Alt+Del sign-in has been enabled!
+echo.
+timeout 5
+goto tweaks
+
+
 :d1
 cls
 title WinScripts - applying the tweak..
@@ -354,6 +242,19 @@ echo.
 timeout 5
 goto tweaks
 
+:d4
+cls
+title WinScripts - applying the tweak..
+echo.
+echo. Disabling Ctrl+Alt+Del sign-in on Windows lock screen..
+echo.
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v "DisableCAD" /t REG_DWORD /d 0 /f
+echo.
+echo. Ctrl+Alt+Del sign-in has been disabled!
+echo.
+timeout 5
+goto tweaks
+
 :scripts
 cls
 title WinScripts - PowerShell scripts
@@ -366,6 +267,7 @@ echo. Type in "3" for AveYo edge removal
 echo. Type in "4" to download WPD + ShutUp10++ to your documents
 echo. Type in "5" to install all VC Runtimes (my mirror)
 echo. Type in "6" to install Chocolatey
+echo. Type in "7" for Win11Debloat
 echo.
 echo. Type in "start" to go to the main menu
 echo. Type in "restart" to restart your machine
@@ -379,7 +281,7 @@ if %script% == 3 goto s3
 if %script% == 4 goto s4
 if %script% == 5 goto s5
 if %script% == 6 goto s6
-if %script% == choco-auto goto choco-auto
+if %script% == 7 goto s7
 if %script% == start goto start
 if %script% == restart goto restart
 if %script% == exit goto exit
@@ -421,6 +323,10 @@ echo.
 echo. After installation use "choco feature enable -n=allowGlobalConfirmation" in a new CMD window to automatially accept prompts when installing packages in Chocolatey.
 echo.
 pause
+goto scripts
+
+:s7
+start PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& ([scriptblock]::Create((irm "https://raw.githubusercontent.com/Raphire/Win11Debloat/master/Get.ps1")))"
 goto scripts
 
 :cpanel
